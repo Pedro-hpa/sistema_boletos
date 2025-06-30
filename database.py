0901c0af4150753223002a976406dc253cb94f8e
pedro henrique <pedro.hq.pereira@outlook.com>
2025-06-30 16:11:32 -0300

    Migração para PostgreSQL no Render

--- database.py
+++ database.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./boletos.db"
+# Usa a variável DATABASE_URL definida no ambiente
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://banco_boletos_user:PDnPFhK7EAvkClhNXlCTYkaK6HCR8ovE@dpg-d1hdvhidbo4c73da42pg-a/banco_boletos")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False)
 Base = declarative_base()
